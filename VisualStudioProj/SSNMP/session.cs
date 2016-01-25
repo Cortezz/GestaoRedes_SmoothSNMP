fee9091ebc0401e71471ebf19a4f2b59d63ac981
José Cortez <Jospcortez@gmail.com>
2016-01-25 23:42:13 +0000

    Revert "SNMP Get is working."
    This reverts commit 863026aaf02c5443ecf0401d644a43baac0c87ae.

--- VisualStudioProj/SSNMP/session.cs
+++ VisualStudioProj/SSNMP/session.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +23,7 @@ namespace SmoothSNMP
         /// </summary>
         public Session()
         {
-            this.host = "127.0.0.1";
+            this.host = "localhost";
             this.port = 161;
             this.community = "public";
         }
@@ -79,26 +77,20 @@ namespace SmoothSNMP
         {
             
             PDU pdu = new PDU(1024);
-            byte[] data =  pdu.buildPDU(0, 0, this.community, mibs);
-            byte[] response = SendPDU(data, pdu.getIndex() + 1);
-            return response;
+            byte[] data =  pdu.buildPDU(0, 1, this.community, mibs);
+            for (int i = 0; i < data.Length; i++)
+                Console.WriteLine(i + " - " + data[i].ToString("X"));
+            return data;
 
         }
 
-
-        private byte[] SendPDU(byte[] PDU, int size)
+        public static void main (String[] args)
         {
-            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(host), 161);
-            UdpClient udpClient = new UdpClient();
-            udpClient.Connect(iep);
-            udpClient.Send(PDU, size);
-            
-            try
-            {
-                byte[] response = udpClient.Receive(ref iep);
-                return response;
-            }
-            catch (Exception e) { Console.WriteLine(e.ToString()); return null; }
+            string[] mibs = new string[1] { "1.2.3.4.5.6.7" };
+            Session session = new Session();
+            byte[] pdu = session.get(mibs);
+            for (int i = 0; i<pdu.Length;i++)
+                Console.WriteLine(i+" - "+pdu[i].ToString("X"));
         }
         
     }
